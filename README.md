baf4533803bf9f3bf966e98a8cacaeb408ec302a
robbiemie <yang657850144@163.com>
2021-12-02 22:15:37 +0800

    feat: 命令行编译 *.swift 文件

---
+++ README.md
@@ -0,0 +1,5 @@
+# 命令行编译 swift 文件
+
+```bash
+$ swiftc -o test.out test.swift
+```
---
+++ test/test.swift
@@ -0,0 +1,7 @@
+func addTwoNum(num1: Int, num2: Int) -> Int {
+    return num1 + num2
+}
+
+let result = addTwoNum(num1: 1, num2: 2)
+
+print(result)
